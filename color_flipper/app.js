9cadc4ff27900c2e6abfb718654879cc9184aa2d
d-ahn10 <no-reply.email.address>
2022-03-16 17:59:46 -0700

    fixed duplicate colors on click for simple picker

--- color_flipper/app.js
+++ color_flipper/app.js
@@ -3,13 +3,23 @@ const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function() {
-    // get random number between 0 - 3 colors[2]
     const randomNumber = getRandomNumber();
-    console.log('here: ', document.body.style);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+    const dupeColor = checkDuplicateColor(colors[randomNumber], document.body.style.cssText);
+
+    if (!dupeColor) {
+        document.body.style.backgroundColor = colors[randomNumber];
+        color.textContent = colors[randomNumber];
+    } else {
+        const randomNumber = getRandomNumber();
+        document.body.style.backgroundColor = colors[randomNumber];
+        color.textContent = colors[randomNumber];
+    }
 });
 
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
+}
+
+function checkDuplicateColor(colorElement, bgColorText) {
+    return bgColorText.includes(colorElement) ? true : false;
 }
